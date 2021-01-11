2d68e42c93a353c567b67d0ba2eee857be7200bc
xionglei <xionglei@metrodata.cn>
2021-01-11 17:36:45 +0800

    update scheduled time rule

--- src/main/java/com/example/demo/task/FirstTask.java
+++ src/main/java/com/example/demo/task/FirstTask.java
@@ -7,6 +7,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 @Component
@@ -14,24 +16,30 @@ import org.springframework.stereotype.Component;
 public class FirstTask {
     Logger logger = LoggerFactory.getLogger(FirstTask.class);
 
-    @Scheduled(fixedRate = 1000*3)
+    @Scheduled(cron = "*/5 * * * * ?")
     @Async
     public void starttask(){
         logger.info("===开始调度程序===");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH24:mm:ss");
+        System.out.println("11111"+simpleDateFormat.format(new Date()));
         logger.info("===结束一次调度===");
     }
 
-    @Scheduled(fixedRate = 1000*3)
+    @Scheduled(cron = "*/10 * * * * ?")
     @Async
     public void starttask2(){
         logger.info("===开始调度程序2===");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH24:mm:ss");
+        System.out.println("22222"+simpleDateFormat.format(new Date()));
         logger.info("===结束一次调度2===");
     }
 
-    @Scheduled(fixedRate = 1000*3)
+    @Scheduled(fixedRate = 1000*6)
     @Async
     public void starttask3(){
         logger.info("===开始调度程序3===");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH24:mm:ss");
+        System.out.println("333333"+simpleDateFormat.format(new Date()));
         logger.info("===结束一次调度3===");
     }
 }
