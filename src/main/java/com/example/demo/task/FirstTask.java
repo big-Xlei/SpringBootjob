2f654427bc31627dddd3b74371877f5736d08f66
xionglei <xionglei@metrodata.cn>
2021-01-11 15:50:33 +0800

    add async

--- src/main/java/com/example/demo/task/FirstTask.java
+++ src/main/java/com/example/demo/task/FirstTask.java
@@ -3,7 +3,6 @@ package com.example.demo.task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,19 +11,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
-@EnableAsync
 public class FirstTask {
     Logger logger = LoggerFactory.getLogger(FirstTask.class);
-    @Scheduled(fixedRate = 1000*3)
 
+    @Scheduled(fixedRate = 1000*3)
     @Async
     public void starttask(){
         logger.info("===开始调度程序===");
-
-
-
-
         logger.info("===结束一次调度===");
     }
 
+    @Scheduled(fixedRate = 1000*3)
+    @Async
+    public void starttask2(){
+        logger.info("===开始调度程序2===");
+        logger.info("===结束一次调度2===");
+    }
+
+    @Scheduled(fixedRate = 1000*3)
+    @Async
+    public void starttask3(){
+        logger.info("===开始调度程序3===");
+        logger.info("===结束一次调度3===");
+    }
 }
